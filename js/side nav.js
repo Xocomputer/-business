f9d9c0ffad9d04894c58023cfec567ce60fa373c
Xocomputer <103369700+Xocomputer@users.noreply.github.com>
2022-04-15 17:08:02 +0300

    Create side nav.js

---
+++ js/side nav.js
@@ -0,0 +1,10 @@
+ function openNav(){
+            document.getElementById("sideNav").style.width="18%";
+            //document.body.style.marginLeft="18%";
+            document.body.style.backgroundColor="rgba(0,0,0,0.7)";
+        }
+         function closeNav(){
+            document.getElementById("sideNav").style.width="0";
+            document.body.style.marginLeft="0";
+            document.body.style.backgroundColor="white";
+        }
